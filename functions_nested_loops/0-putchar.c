ee7d6f920f17b78a029f4944a280c7fec77e72b3
Epicder <epicder11@gmail.com>
2024-02-15 21:18:47 +0000

    commit

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 int main(void)
-{  
+{
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -10,6 +10,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('\n'); 
- return (0);
+_putchar('\n');
+return (0);
 }
