57ba027163f8c3fbcd55606495b2c37f5afdfb60
Epicder <epicder11@gmail.com>
2024-02-15 15:11:13 +0000

    commit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,11 +11,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	    printf("%i is positive", n);
+	    printf("%d is positive", n);
 	  else if (n == 0)
-	  printf("%i is zero", n);
+	  printf("%d is zero", n);
 	  else
-	    printf("%i is postitive", n);
+	    printf("%d is postitive", n);
 	
          
 	  
