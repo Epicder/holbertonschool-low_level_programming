f36f20f31738c29c918657fc4c5a5ee9a6d55b75
Epicder <epicder11@gmail.com>
2024-02-19 20:35:32 +0000

    commit

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include <stdio.h>
 /**
  * add - check the code
  *
- * Return: Always 0.
+ * Return: devuelve la suma
  */
 int add(int a, int b)
 {
-printf("%d\n", a + b);
+  return (a + b);
 }
