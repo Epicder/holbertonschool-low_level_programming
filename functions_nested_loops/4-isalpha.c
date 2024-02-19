aa96eb2737adac189af686b5bc2d247c896b22bb
Epicder <epicder11@gmail.com>
2024-02-19 02:15:55 +0000

    commit

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * @c: verifica si la itegral se encuentra en el abcdario xdd
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
