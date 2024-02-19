a2c34010dbb2f816321a3bf289adc59e23c37142
Epicder <epicder11@gmail.com>
2024-02-19 23:07:02 +0000

    commit

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,16 @@
 
 void more_numbers(void)
 {
-  int a;
-  int b;
-  for (a = 0; a < 10; a++)
-    {
+int a;
+int b;
+for (a = 0; a < 10; a++)
+{
 for (b = 0; b < 15; b++)
 {
 if (b >= 10)
 _putchar(b / 10 + '0');
 _putchar(b % 10 + '0');
 }
-}
 _putchar('\n');
+}
 }
