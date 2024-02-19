add0d2cfe26621f61235e51e75e3ae21d5223738
Epicder <epicder11@gmail.com>
2024-02-19 22:41:09 +0000

    commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - check the code
+ * @a: primer integral
+ * @b: int to be multiplied to a
+ * Return: la operacion
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
