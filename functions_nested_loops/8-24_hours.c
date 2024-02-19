5f039391c447f0e602948a8e8bebd3467bb66296
Epicder <epicder11@gmail.com>
2024-02-19 19:49:30 +0000

    commit

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -7,7 +7,7 @@
 
 void jack_bauer(void)
 {
-  int hora1, hora2;
+int hora1, hora2;
 for (hora1 = 0; hora1 < 24; hora1++)
 {
 for (hora2 = 0; hora2 < 60; hora2++)
@@ -19,6 +19,6 @@ _putchar((hora2 / 10) + '0');
 _putchar((hora2 % 10) + '0');
 _putchar('\n');
 }
-     }
+}
 }
 
