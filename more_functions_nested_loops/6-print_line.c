0f7068c4976c8d096a3c9996ed5dc793f290f87d
Epicder <epicder11@gmail.com>
2024-02-20 00:06:55 +0000

    commit

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_line - check the code
- *
+ * @n: variables
  * Return: Always 0.
  */
 void print_line(int n)
@@ -9,7 +9,8 @@ void print_line(int n)
 if (n <= 0)
 {
 _putchar('\n');
-}else
+}
+else
 {
 int i;
 for (i = 1; i <= n; i++)
