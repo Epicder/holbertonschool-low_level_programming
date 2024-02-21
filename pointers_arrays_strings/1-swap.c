45f7e2a162e582561a742aca1653b9efe1291799
Epicder <epicder11@gmail.com>
2024-02-21 12:56:39 +0000

    commit

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * main - check the code 
+ * swap_int - check the code 
  * 
  * Return: Always 0.
  */
 
 void swap_int(int *a, int *b)
 {
-  int c;
-  c = *a;
-  *a = *b;
-  *b = c;
+int c;
+c = *a;
+*a = *b;
+*b = c;
 }
