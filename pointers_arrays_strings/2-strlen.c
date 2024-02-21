54a798faaab26c5bcd8c1ae5d99a3a9f146293f5
Epicder <epicder11@gmail.com>
2024-02-21 13:07:27 +0000

    commit

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - check the code 
+ * @s: variable
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+  int a;
+  a = strlen(s);
+return (a);
+}
