3f06dc270950a16e1ec26e8fe35e94db07efc8f7
Epicder <epicder11@gmail.com>
2024-02-22 14:32:50 +0000

    commit

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,26 +1,29 @@
 #include "main.h"
-
+/**
+ * puts_half - printea la segunda mitad de un string
+ * @str: la string a la cual solo le printeamos la 2da mitad
+ * Return: Always 0.
+ */
 void puts_half(char *str)
 {
-  int a = 0;
-  int b;
-  while (str[a] != '\0')
-    {
-      a++;
-    }
-
-  if ( a % 2 == 1)
-    {
-      b = (a - 1) / 2;
-      b += 1;
-   }
-  else if (a % 2 == 0)
-    {
-      b = a / 2;
-    }
-  for (; b <= a ; b++)
-    {
-      _putchar(str[b]);
-    }
-  _putchar('\n');
+int a = 0;
+int b;
+while (str[a] != '\0')
+{
+a++;
+}
+if (a % 2 == 1)
+{
+b = (a - 1) / 2;
+b += 1;
+}
+else
+{
+b = a / 2;
+}
+for (; b <= a ; b++)
+{
+_putchar(str[b]);
+}
+_putchar('\n');
 }
