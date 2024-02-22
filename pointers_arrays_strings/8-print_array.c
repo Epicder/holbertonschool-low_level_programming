cd32b4fccf3eebafc3014eb7becb36f222795b8e
Epicder <epicder11@gmail.com>
2024-02-22 17:54:28 +0000

    commit

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,21 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code for
- *
+ * print_array - funcion la cual printea cada valor de un array
+ * @a: array
+ * @n: valor de la array
  * Return: Always 0.
  */
 void print_array(int *a, int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-	  printf("%i", a[i]);
-	  if (i != (n - 1))
-		{
-		  printf(", ");
-		}
-	}
-	printf("\n");
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%i", a[i]);
+if (i != (n - 1))
+{
+printf(", ");
+}
+}
+printf("\n");
 }
