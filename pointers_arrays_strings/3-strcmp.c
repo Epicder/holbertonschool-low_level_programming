be23f5a33b2ea60490593aacf9c926667a7092c9
Epicder <epicder11@gmail.com>
2024-02-26 17:22:26 +0000

    commit

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -17,16 +17,16 @@ return s1[i] - s2[i];
 }
 i++;
 }
-  if (s1[i] == '\0' && s2[i] == '\0')
-    {
-      return (0);
-    }
-  else if (s1[i] == '\0')
-    {
-      return (-1);
-    }
-  else
-    {
-      return (1);
-    }
+if (s1[i] == '\0' && s2[i] == '\0')
+{
+return (0);
+}
+else if (s1[i] == '\0')
+{
+return (-1);
+}
+else
+{
+return (1);
+}
 }
