8bfeb6dfbc54a9c03a7b525feab67cc32dc6ca95
Epicder <epicder11@gmail.com>
2024-02-26 18:21:39 +0000

    commit

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
  * string_toupper - cambia todas las minusculas a mayusculas
- * @str: string a ser mayusculizada xD 
+ * @str: string a ser mayusculizada xD
  * Return: Always 0.
  */
-
 char *string_toupper(char *str)
 {
 int i;
@@ -12,7 +11,7 @@ for (i = 0; str[i] != '\0'; i++)
 {
 if (str[i] >= 'a' && str[i] <= 'z')
 {
-str[i] = str[i] - 32;  
+str[i] = str[i] - 32;
 }
 }
 return (str);
