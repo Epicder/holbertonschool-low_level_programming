37f9b557363a5bdeaee9b696c2ec29e397dcf1aa
Epicder <epicder11@gmail.com>
2024-02-26 17:21:05 +0000

    commit

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,23 +8,25 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-  int i;
-  int a;
-  while (s1[i] != '\0')
-    {
-      i++;
-    }
-  while (s2[a] != '\0')
-    {
-      a++;
-    }
-  if (s1[i] == s2[a])
+int i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+{
+return s1[i] - s2[i];
+}
+i++;
+}
+  if (s1[i] == '\0' && s2[i] == '\0')
     {
       return (0);
     }
-  else if (s1[i] < s2[a])
+  else if (s1[i] == '\0')
     {
       return (-1);
     }
-  return (1);
+  else
+    {
+      return (1);
+    }
 }
