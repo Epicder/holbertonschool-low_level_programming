1efe25fa51e277cafe33af2bfb4449c7f0b9d2ef
Epicder <epicder11@gmail.com>
2024-02-26 03:19:50 +0000

    commit

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -4,17 +4,25 @@
  * @dest: destinatario
  * @src: fuente
  * @n: bytes
- * este codigo lo saque de hacer man strncat, entiendo la logica menos el tipo
- * de variable size_t, no se hasta que punto se pueda hacer esto?
  * Return: Always 0.
  */
 
-char *_strncat(char *dest, const char *src, size_t n)
+char *_strncat(char *dest, char *src, int n)
 {
-size_t dest_len = strlen(dest);
-size_t i;
-for (i = 0 ; i < n && src[i] != '\0' ; i++)
-dest[dest_len + i] = src[i];
-dest[dest_len + i] = '\0';
+int j;
+int i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+j++;
+}
+for (; j < n;)
+{
+dest[i + j] = src[j];
+}
+dest[i + j] = '\0';
 return (dest);
 }
