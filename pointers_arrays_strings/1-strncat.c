2a53a026de13b852880e47d3ad912981f073f1db
Epicder <epicder11@gmail.com>
2024-02-26 03:23:58 +0000

    commit

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,19 +9,16 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int j;
+int j = 0;
 int i = 0;
 while (dest[i] != '\0')
 {
 i++;
 }
-while (src[j] != '\0')
-{
-j++;
-}
-for (; j < n; j++)
+while (src[j] != '\0' && j < n)
 {
 dest[i + j] = src[j];
+j++;
 }
 dest[i + j] = '\0';
 return (dest);
