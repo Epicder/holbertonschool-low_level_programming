043510835e65606e67268cea2f4fe35410baf25e
Epicder <epicder11@gmail.com>
2024-02-27 21:50:39 +0000

    commit

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * strpbrk - Write a function that searches a string for any of a set of bytes.
+ * @s: string
+ * @accept: string
+ * Return: retorna un puntero a s si coiniciden los bytes con accept, sino NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+while (*s != '\0')
+{
+char *puntero = accept;
+while (*puntero != '\0')
+{
+if (*s == *puntero)
+{
+return (s);
+}
+puntero++;
+}
+s++;
+}
+return ('\0');
+}
