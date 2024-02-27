5b56c5da9a5477bdcbf80fefb7832a84bdc75ebc
Epicder <epicder11@gmail.com>
2024-02-27 21:19:23 +0000

    commit

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -3,24 +3,28 @@
 /**
  * _strspn - consigue la longitud de un prefijo
  * @s: string a ser utilizada
- * @accept: 
+ * @accept: valor a ser tomado
  * Return: Always 0.
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i = 0;   
-while (*s != '\0') {
-char *puntero = accept;	
-while (*puntero != '\0') {
-if (*s == *puntero) {
+unsigned int i = 0;
+while (*s != '\0')
+{
+char *puntero = accept;
+while (*puntero != '\0')
+{
+if (*s == *puntero)
+{
 i++;
-break; 
+break;
 }
 puntero++;
 }
-if (*puntero == '\0') {
-return (i); 
+if (*puntero == '\0')
+{
+return (i);
 }
 s++;
 }
