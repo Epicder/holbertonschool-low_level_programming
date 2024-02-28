448cc57fab285618628cfc781d2cf070b8a3135b
Epicder <epicder11@gmail.com>
2024-02-28 00:23:48 +0000

    commit

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagsums - check the code
+ * @a: variable
+ * @size: tamano
+ * Return: Always 0.
+ */
+void print_diagsums(int *a, int size)
+{
+int i;
+int b;
+b = *a + size;
+for(i = 0; i < 6; i++)
+{
+b++;
+}
+/*lo que me ensenian: 1+1=2
+lo que piden en el examen: Printea la suma de dos diagonales de una
+matriz cuadrada de numeros enteros xdd */
+}
