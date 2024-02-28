2e747187f5fb73d1caceaafeb9f6854f41a3cbf3
Epicder <epicder11@gmail.com>
2024-02-28 02:25:51 +0000

    commit

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
  * main - check the code
- *
+ * @s string a ser analizada
  * Return: Always 0.
  */
 int _strlen_recursion(char *s)
 {
-int i = 0;
 if (*s == '\0')
 {
 return (0);
 }
-i++;
 return (1) +_strlen_recursion(s + 1);
 }
