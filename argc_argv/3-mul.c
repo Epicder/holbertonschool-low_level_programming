d496e3bd9cdd41713459cfa1595e560928bd5762
Epicder <epicder11@gmail.com>
2024-02-28 16:30:18 +0000

    commit

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * 
- *
- *
- *
+ * main - multiplica 2 argumentos
+ * @argc: cantidad de argumenots
+ * @argv: valor de los argumentos
+ * Return: 0 o 1
  */
 int main(int argc, char *argv[])
 {
-  int a;
-  int b;
- int multi;
- if (argc != 3)
-   {
-     printf("Error\n");
-     return (1);
-   }
- a = atoi(argv[1]);
-     b = atoi(argv[2]);
-       multi = a * b;
-     printf("%d\n", multi);
+int a;
+int b;
+int multi;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+multi = a *b;
+printf("%d\n", multi);
 return (0);
 }
- 
