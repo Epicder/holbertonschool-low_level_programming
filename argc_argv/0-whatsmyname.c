6ca0dc38ec15b49b4af33f6e90c1f2411354efb2
Epicder <epicder11@gmail.com>
2024-02-28 13:35:24 +0000

    commit

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - printea el nombre del archivo
+ * @argv: nombre del archivo
+ * @argc: cantidad de argumentos
+ * Return: Nombre del archivo
+ */
+int main (int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
