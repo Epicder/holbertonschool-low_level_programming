f52cff28740e4a070565c0263adb645807e1aca8
Epicder <epicder11@gmail.com>
2024-02-28 16:26:28 +0000

    commit

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -11,16 +11,14 @@ int main(int argc, char *argv[])
   int a;
   int b;
  int multi;
- if (argc > 1)
-   {
-     a = atoi(argv[1]);
-     b = atoi(argv[2]);
-       multi = a * b;
-     printf("%d\n", multi);
-   }
- else
+ if (argc != 3)
    {
      printf("Error\n");
    }
- return (0);
+ a = atoi(argv[1]);
+     b = atoi(argv[2]);
+       multi = a * b;
+     printf("%d\n", multi);
+return (0);
 }
+ 
