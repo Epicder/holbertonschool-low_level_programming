5dc65b66e5e0bf7341c9ca749b481655b3e0bb8c
Epicder <epicder11@gmail.com>
2024-02-29 14:53:25 +0000

    commit

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - sumar argumentos dados
+ * @argc: cantidad de argumentos
+ * @argv: valor de lso argumentos
+ * Return: 0 o 1
+ */
 int main(int argc, char *argv[])
 {
-  int i;
-  int suma;
-    if (argc == 1)
-    {
-      printf("0\n");
-    }
-    else
-      {
-    for (i = 1; i < argc; i++)
-	    {
-          suma += atoi(argv[i]);
-	    }
-       printf("%d\n", suma);
-      }
-  return (0);
-}  
+int a;
+int suma;
+if (argc == 1)
+{
+printf("0\n");
+}
+else
+{
+for (a = 1; a < argc; a++)
+{
+int num = atoi(argv[a]);
+if (num == 0 && *argv[a] != '0')
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+suma += atoi(argv[a]);
+}
+}
+printf("%d", suma);
+}
+return (0);
+}
