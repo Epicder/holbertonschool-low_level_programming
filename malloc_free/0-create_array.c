00280f94914e89863f897570cc67d9b1d119377c
Epicder <epicder11@gmail.com>
2024-03-01 14:01:46 +0000

    commit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -8,22 +8,20 @@
  */
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i = 0;
-  char *array;
-  if (size == 0)
-    {
-      return (NULL);
-    }
+unsigned int i = 0;
+char *array;
+if (size == 0)
+{
+return (NULL);
+}
 array = (char *)malloc(size * sizeof(char));
-
- if (array == NULL)
-   {
-     return (NULL);
-   }
-
- for (i = 0; i < size; i++)
-   {
-     array[i] = c;   
-   }
- return (array);
+if (array == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+array[i] = c;   
+}
+return (array);
 }
