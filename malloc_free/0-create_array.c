ed861d453891b1b6d3a39fa60db7b94a990f9bce
Epicder <epicder11@gmail.com>
2024-03-01 14:00:57 +0000

    commit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ * create_array - crea un array de chars
+ * @size: tamanio del array
+ * @c: array
+ * Return: NULL o el array
  */
 char *create_array(unsigned int size, char c)
 {
