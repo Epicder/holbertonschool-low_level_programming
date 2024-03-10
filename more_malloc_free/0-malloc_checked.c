e976920a59765b685d474b55564eb4e7e2c1dcba
Epicder <epicder11@gmail.com>
2024-03-10 01:49:37 +0000

    commit

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - localiza la memoria alocada en b
+ * @b: variable para malloc
+ * Return: puntero
+ */
+void *malloc_checked(unsigned int b);
+{
+void *puntero = malloc(b);
+if (puntero == NULL)
+{
+exit(98);
+}
+return (puntero);
+}
