9253c7820e7809971d8ea8cc5536d123a8b00dbc
Epicder <epicder11@gmail.com>
2024-03-11 13:12:45 +0000

    commit

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * init_dog - inicia los valores de la estructura
+ * @d: dog
+ * @name: nombre
+ * @age: edad
+ * @owner: duenio
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
