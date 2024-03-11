3a7fcbb2cfa5d51c55e2008112db07fb125ff8ae
Epicder <epicder11@gmail.com>
2024-03-11 17:28:00 +0000

    commit

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,21 +1,23 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ * new_dog - crea una nueva estructura
+ * @name: nombre
+ * @age: edad
+ * @owner: duenio
+ * Return: puntero a la nueva estructura
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-  struct dog *new_dog;
-  new_dog = malloc(sizeof(struct dog));
-  if (new_dog == NULL)
-    {
-      return (NULL);
-    }
- new_dog->name = name;
- new_dog->age = age;
- new_dog->owner = owner;
- return(new_dog);
+struct dog *new_dog;
+new_dog = malloc(sizeof(struct dog));
+if (new_dog == NULL)
+{
+free(new_dog);
+return (NULL);
+}
+new_dog->name = name;
+new_dog->age = age;
+new_dog->owner = owner;
+return (new_dog);
 }
