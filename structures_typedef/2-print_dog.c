153f6ba63cbbb0d5c04554ffb3e357943ab21b0b
Epicder <epicder11@gmail.com>
2024-03-11 14:10:12 +0000

    commit

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -7,32 +7,10 @@
  */
 void print_dog(struct dog *d)
 {
-  if (d == NULL)
-    {
-      
-    }
-  if (d->name == NULL)
-    {
-  printf("Name: (nil)\n");
-  printf("Age: %f\n", d->age);
-  printf("Owner: %s\n", d->owner);
-    }
-  else if (d->owner == NULL)
-    {
-printf("Name: %s\n", d->name);
-  printf("Age: %f\n", d->age);
-  printf("Owner: (nil)");
-    }
-  else if (d->owner == NULL && d->name == NULL)
-    {
-printf("Name: (nil)\n");
-  printf("Age: %f\n", d->age);
-  printf("Owner: (nil)");
-    }
-  else
-    {
-  printf("Name: %s\n", d->name);
-  printf("Age: %f\n", d->age);
-  printf("Owner: %s\n", d->owner);
-    }
+if (d == NULL)
+{
+}
+printf("Name: %s\n", d->name ? d->name : "(nil)");
+printf("Age: %s\n", d->age ? d->age : "(nil)");
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");    
 }
