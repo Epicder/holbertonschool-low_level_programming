fff649009e1d87b5439d791f1ae760d59a87f9e3
Epicder <epicder11@gmail.com>
2024-03-13 20:55:46 +0000

    commit

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,8 +13,8 @@ unsigned int i;
 va_start(nums, n);
 for (i = 0; i < n; i++)
 {
-printf("%d, ", va_arg(nums, int));
-if (i != (n - 1) && !separator)
+printf("%d", va_arg(nums, int));
+if (separator != NULL && i != (n - 1))
 {
 printf("%s", separator);
 }
