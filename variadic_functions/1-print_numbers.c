692fc4901ef60aed7d2fb87d439db3681df98b26
Epicder <epicder11@gmail.com>
2024-03-13 02:58:43 +0000

    commit

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - printea numeros
+ * @separator: la string a ser printeada entre los nums
+ * @n: numero de ints
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list nums;
+unsigned int i;
+va_start(nums, n);
+for (i = 0; i < n; i++)
+{
+printf("%d, ", va_arg(nums, int));
+if (i != (n - 1) && !separator)
+{
+printf("%s", separator);
+}
+}
+printf("\n");
+va_end(nums);
+}
