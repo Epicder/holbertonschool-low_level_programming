1178ead319c3a55dbefdd94e5275fc6c3a8fb871
Epicder <epicder11@gmail.com>
2024-03-25 14:42:39 +0000

    commit

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,12 +1,17 @@
 #ifndef SINGLY_LISTS
 #define SINGLY_LISTS
 #include <stddef.h>
-
+/**
+ * struct list_a - lista de argumentos
+ * @str: string
+ * @len: longitud
+ * @next: pointer al siguiente nodo
+ */
 typedef struct list_a
 {
-    char *str;
-    size_t len;
-    struct list_a *next;
+char *str;
+size_t len;
+struct list_a *next;
 } list_t;
 
 size_t print_list(const list_t *h);
